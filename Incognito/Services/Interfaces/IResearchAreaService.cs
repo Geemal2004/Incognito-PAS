97620c2ed92257d9f66c19721f58bfd2e2c9f771
BinuraGamage <binuragamage818@gmail.com>
2026-04-19 11:12:07 +0530

    feat(matching-engine): construct high-fidelity mapping algorithms and module leader APIs

---
+++ Incognito/Services/Interfaces/IResearchAreaService.cs
@@ -0,0 +1,12 @@
+using IncognitoPAS.Models;
+
+namespace IncognitoPAS.Services.Interfaces;
+
+public interface IResearchAreaService
+{
+    Task<IEnumerable<ResearchArea>> GetAllAsync();
+    Task<ResearchArea?> GetByIdAsync(int id);
+    Task<ResearchArea> CreateAsync(ResearchArea area);
+    Task<ResearchArea?> UpdateAsync(ResearchArea area);
+    Task<bool> DeleteAsync(int id);
+}
